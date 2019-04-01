b17123cad25edfd826d9020e0ff0baf401a807b1
Robert Nelson <reg0204@sys-service.de>
2019-04-01 17:17:10 +0200

    1.4.17.17

--- SimpleShoppingList/Scripts/app.js
+++ SimpleShoppingList/Scripts/app.js
@@ -19,6 +19,21 @@ function addItem() {
     newItem.name = $("#newItemName").val();
     currentList.items.push(newItem);
     console.info(currentList);
+
+    drawItems();
+}
+
+function drawItems() {
+    var $list = $("#shoppingListItems").empty();
+
+    for (var i = 0; i < currentList.items; i++) {
+        var currentItem = currentList.items[i];
+        var $li = $("<li>").html(currentItem.name)
+            .attr("id", "item_" + i);
+        var $deleteBtn = $("<button>D<button>").appendTo($li);
+        var $checkBtn = $("<button>C<button>").appendTo($li);
+        $li.appendTo($list);
+    }
 }
 
 $(document).ready(function () {
