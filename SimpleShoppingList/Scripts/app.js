3ad0c842b108d05934ba83e55dd399e664fbaa5d
Nelson <Robert.Nelson@allgeier-experts.com>
2019-04-01 12:46:10 +0200

    first commit

---
+++ SimpleShoppingList/Scripts/app.js
@@ -0,0 +1,19 @@
+﻿var currentList { };
+
+function createShoppingList() {
+    currentList.name = $("#shoppingListName").val();
+
+    // web service call
+
+    $("#shoppingListTitle").html(currentList.name);
+    $("#shoppingListItems").empty();
+
+    $("#createListDiv").hide();
+    $("#shoppingListDiv").show();
+
+}
+
+
+$(document).ready(function () {
+    console.info("ready");
+});
