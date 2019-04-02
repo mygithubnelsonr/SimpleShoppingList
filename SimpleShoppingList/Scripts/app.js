4a4dd70e9eba2377d0a9cf1db7b577462a3227ac
Robert Nelson <reg0204@sys-service.de>
2019-04-02 15:12:19 +0200

    2019.04.02.15.11

--- SimpleShoppingList/Scripts/app.js
+++ SimpleShoppingList/Scripts/app.js
@@ -6,36 +6,92 @@ function createShoppingList() {
 
     // web service call
 
+    showShoppingList();
+
+}
+
+function showShoppingList() {
     $("#shoppingListTitle").html(currentList.name);
     $("#shoppingListItems").empty();
 
     $("#createListDiv").hide();
     $("#shoppingListDiv").show();
 
+    $("#newItemName").focus();
+    $("#newItemName").keyup(function (event) {
+        if (event.keyCode == 13) {
+            addItem();
+        }
+    });
 }
 
 function addItem() {
     var newItem = {};
     newItem.name = $("#newItemName").val();
     currentList.items.push(newItem);
-    console.info(currentList);
+    // console.info(currentList);
 
     drawItems();
+
+    $("#newItemName").val("");
 }
 
 function drawItems() {
     var $list = $("#shoppingListItems").empty();
-
-    for (var i = 0; i < currentList.items; i++) {
+    // console.info("Listen Einträge: " + currentList.items.length);
+    for (var i = 0; i < currentList.items.length; i++) {
         var currentItem = currentList.items[i];
         var $li = $("<li>").html(currentItem.name)
             .attr("id", "item_" + i);
-        var $deleteBtn = $("<button>D<button>").appendTo($li);
-        var $checkBtn = $("<button>C<button>").appendTo($li);
+        var $deleteBtn = $("<button onclick='deleteItem(" + i + ")'>D</button>").appendTo($li);
+        var $checkBtn = $("<button onclick='checkItem(" + i + ")'>C</button>").appendTo($li);
         $li.appendTo($list);
     }
 }
 
+function deleteItem(index) {
+    currentList.items.splice(index, 1);
+    drawItems();
+}
+
+function checkItem(index) {
+    if ($("#item_" + index).hasClass("checked")) {
+        $("#item_" + index).removeClass("checked");
+    }
+    else {
+        $("#item_" + index).addClass("checked");
+    }
+}
+
+function getShoppingListByID(id) {
+    console.info("shopping List ID=" + id);
+
+    currentList.name = "Mock Shopping List";
+    currentList.items = [
+        { name: "Milk" },
+        { name: "Bred" },
+        { name: "Butter"}
+    ];
+
+    showShoppingList();
+    drawItems();
+}
+
 $(document).ready(function () {
     console.info("ready");
+
+    $("#shoppingListName").focus();
+    $("#shoppingListName").keyup(function (event) {
+        if (event.keyCode == 13) {
+            createShoppingList();
+        }
+    });
+
+    var pageUrl = window.location.href;
+    var idIndex = pageUrl.indexOf("?id=");
+
+    if (idIndex != -1) {
+        getShoppingListByID(pageUrl.substring(idIndex + 4));
+    }
+
 })
